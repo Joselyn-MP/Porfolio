927b76401146e21aa21aa65a055e0266bf9fd770
joselyn MP <joselynmonteroperez750@gmail.com>
2026-03-07 21:21:04 -0400

    responsivefoto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,20 +10,23 @@ function App() {
     <>
       <div className="layout">
         <div className="content">
-          <h1>Joselyn Montero Perez</h1>
-          <h3>Desarrolladora de Software</h3>
-          <p>
-              Apasionada por la tecnología y el desarrollo de software.
-          </p>
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
+          <div className="content-left">
+            <h1 className="content-name">Joselyn Montero Perez</h1>
+            <div className="content-body">
+              <h3>Desarrolladora de Software</h3>
+              <p>
+                Apasionada por la tecnología y el desarrollo de software.
+              </p>
+              <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                  count is {count}
+                </button>
+              </div>
+            </div>
           </div>
-          
-        </div>
-        <div className="sphere-container">
-          <img src={miFoto} alt="Joselyn" className="sphere" />
+          <div className="sphere-container">
+            <img src={miFoto} alt="Joselyn" className="sphere" />
+          </div>
         </div>
       </div>
     </>
